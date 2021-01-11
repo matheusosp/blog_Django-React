f6938ccad97df84c1b353cf5f056abfde67f4ad6
matheusosp <matheusosp@users.noreply.github.com>
2021-01-11 19:03:56 -0300

    Adding token and additionnal headers to the fetch call

--- frontend/src/UserList.tsx
+++ frontend/src/UserList.tsx
@@ -1,11 +1,29 @@
 import React from "react";
 import ListComponent from "./ListComponent";
 
-export default function UserList(){
-    return(
-        <div>
-            <ListComponent listName={'Minha Lista'}/>
-            <ListComponent listName={'Minha Lista 2'}/>
-        </div>
-    );
-}
+export default class UserList extends React.Component{
+    state = {lists: null, loading: true};
+    async componentDidMount(){
+        const config = {
+            headers:{
+                'Content-Type':'application/json',
+                'Authorization': 'Token e2696413687923506eb1def2f1098f1935f94783'
+            }
+        };
+         
+        var url = 'http://127.0.0.1:8000/list/';
+        const response = await fetch(url,config);
+        const data = await response.json();
+        console.log(data)
+        this.setState({lists: data, loading: false});
+    }
+
+    render(){
+        return(
+            <div>
+                <ListComponent listName={'Minha Lista'}/>
+                <ListComponent listName={'Minha Lista 2'}/>
+            </div>
+        );
+    }
+    }
