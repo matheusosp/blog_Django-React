fac518c883753ef62951e03a86ac8223cf6189ce
matheusosp <matheusosp@users.noreply.github.com>
2021-01-11 21:20:22 -0300

    filtering the list queryset by the logged user

--- frontend/src/service.tsx
+++ frontend/src/service.tsx
@@ -3,5 +3,5 @@ import axios from "axios";
 
 const APIURL = 'http://127.0.0.1:8000'
 export function fetchProducts(){
-    return axios(`${APIURL}/list`,{headers:{'Content-Type':'application/json','Authorization': 'Token e2696413687923506eb1def2f1098f1935f94783'}})
+    return axios(`${APIURL}/list`,{headers:{'Content-Type':'application/json','Authorization': 'Token 6b46e0c37a24b3d546c33c322a29e32dffd97755'}})
 }
