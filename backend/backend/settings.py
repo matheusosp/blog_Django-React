ceb756d2dbaa4089ae5ca0db3d182a64f7003b12
matheusosp <matheusosp@users.noreply.github.com>
2021-01-12 02:34:51 -0300

    Added static and media files

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -129,3 +129,10 @@ CORS_ORIGIN_WHITELIST = [
     "http://localhost:3000",
     'novoip'
 ]
+
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
