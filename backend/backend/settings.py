549aba046d2b4e7cc6fe6ffa2f025c42dc2f535f
matheusosp <matheusosp@users.noreply.github.com>
2021-01-12 02:26:22 -0300

    Configuring settings and allowed hosts

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'j+2p9eeb%bbh*jst3((-f0a4*dr@+h$_05#=xw$=ze)36jo#xo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '51.143.150.114', 'www.meudominio.com.br']
 
 
 # Application definition
@@ -126,5 +126,6 @@ STATIC_URL = '/static/'
 
 CORS_ORIGIN_WHITELIST = [
     "http://127.0.0.1:3000",
-    "http://localhost:3000"
+    "http://localhost:3000",
+    'novoip'
 ]
